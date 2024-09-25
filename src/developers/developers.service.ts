48d7b1142ca3d9d8e8e8ade72ea28d7318725277
Alexandre Junior <alejunior.dev@gmail.com>
2024-09-24 22:06:52 -0300

    feat: implements developer repository to own file

--- src/developers/developers.service.ts
+++ src/developers/developers.service.ts
@@ -1,26 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDeveloperDto } from './dto/create-developer.dto';
 import { UpdateDeveloperDto } from './dto/update-developer.dto';
+import { Repository } from 'typeorm';
+import { Developer } from './entities/developer.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class DevelopersService {
+  constructor(
+    @InjectRepository(Developer)
+    private readonly repository: Repository<Developer>,
+  ) {}
+
   create(createDeveloperDto: CreateDeveloperDto) {
-    return 'This action adds a new developer';
+    const developer = this.repository.create(createDeveloperDto);
+    return this.repository.save(developer);
   }
 
   findAll() {
-    return `This action returns all developers`;
+    return this.repository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} developer`;
+  findOne(id: string) {
+    return this.repository.findOneBy({ id });
   }
 
-  update(id: number, updateDeveloperDto: UpdateDeveloperDto) {
-    return `This action updates a #${id} developer`;
+  async update(id: string, updateDeveloperDto: UpdateDeveloperDto) {
+    const developer = await this.repository.findOneBy({ id });
+
+    if (!developer) {
+      return null;
+    }
+
+    this.repository.merge(developer, updateDeveloperDto);
+    return this.repository.save(developer);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} developer`;
+  async remove(id: string) {
+    const developer = await this.repository.findOneBy({ id });
+
+    if (!developer) {
+      return null;
+    }
+
+    return this.repository.remove(developer);
   }
 }
